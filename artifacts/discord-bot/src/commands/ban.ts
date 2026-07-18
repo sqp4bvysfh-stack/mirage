b983272d3e4a6c01ff66d021dd0bb5d1fe689fe0
sqp4bvysfh-stack <sqp4bvysfh@privaterelay.appleid.com>
2026-07-18 22:33:54 +0200

    Update ban.ts

--- artifacts/discord-bot/src/commands/ban.ts
+++ artifacts/discord-bot/src/commands/ban.ts
@@ -1,6 +1,7 @@
 import { EmbedBuilder } from "discord.js";
 import type { Command } from "../types.js";
 import { isModerator, canActOn } from "../utils/modCheck.js";
+import { sendServerLog } from "../utils/logs.js";
 
 export const banCommand: Command = {
   name: "ban",
@@ -45,5 +46,6 @@ export const banCommand: Command = {
       )
       .setTimestamp();
     await message.reply({ embeds: [embed] });
+    if (message.guild) await sendServerLog(message.guild, { embeds: [embed] });
   },
 };
